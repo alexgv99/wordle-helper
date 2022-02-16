9b39ef0e12627d4626667dce16e6025f5245e74b
Alex Gouvea Vasconcelos <alexgv@procempa.com.br>
2022-02-16 14:56:30 -0300

    Keyboard events, remove letters/patterns; components for letter/letters; (continue)

--- pages/solver.js
+++ pages/solver.js
@@ -1,6 +1,8 @@
-import { Fragment, useState, useMemo, useCallback } from "react";
+import { Fragment, useState, useEffect, useCallback, useRef } from "react";
+import LettersComponent from "../components/letters-component";
+import useKeypress from "../curstom-hooks/useKeypress";
 
-import styles from "../styles/Solver.module.css";
+import styles from "../styles/Solver.module.scss";
 
 const PLACE_HOLDER = ".....";
 
@@ -11,44 +13,104 @@ function Solver() {
   const [lettersIn, setLettersIn] = useState("");
   const [lettersOut, setLettersOut] = useState("");
   const [regexNot, setRegexNot] = useState([]);
+  const ref = useRef(null);
+  useKeypress("Insert", () => (position !== "?" ? has(true) : null));
+  useKeypress("Delete", () => (position !== "?" ? has(false) : null));
+  useKeypress("Enter", () => (position === "?" ? hasnt() : null));
+  useKeypress("1", () => setPosition("1"));
+  useKeypress("2", () => setPosition("2"));
+  useKeypress("3", () => setPosition("3"));
+  useKeypress("4", () => setPosition("4"));
+  useKeypress("5", () => setPosition("5"));
+  useKeypress("Escape", () => {
+    setPosition("?");
+    setLetter("");
+  });
 
-  const has = useCallback(() => {
-    if (position && letter) {
-      if (position === "?") {
-        if (solved.indexOf(letter) === -1 && lettersIn.indexOf(letter) === -1) {
-          setLettersIn((old) => `${old}${letter}`);
-        }
-      } else {
-        setSolved((old) => {
-          const newSolved = old.split("");
-          newSolved[position - 1] = letter;
-          return newSolved.join("");
-        });
-      }
-      setLetter("");
-      setPosition("?");
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.focus();
     }
-  }, [letter, position, solved, lettersIn]);
+  }, []);
+
+  const removeFromState = useCallback((letter, setter, keepSlot) => {
+    setter((old) =>
+      old.split("").reduce((acc, l) => {
+        if (l !== letter) {
+          acc += l;
+        } else if (keepSlot) {
+          acc += ".";
+        }
+        return acc;
+      }, "")
+    );
+  }, []);
+
+  const handleRemove = useCallback(
+    (setter, keepSlot) => (letter) => {
+      removeFromState(letter, setter, keepSlot);
+      ref.current.focus();
+    },
+    [removeFromState]
+  );
+
+  const has = useCallback(
+    (rightPosition) => {
+      if (position !== "?" && letter) {
+        if (rightPosition) {
+          setSolved((old) => {
+            const newSolved = old.split("");
+            newSolved[position - 1] = letter;
+            return newSolved.join("");
+          });
+          removeFromState(letter, setLettersIn);
+          removeFromState(letter, setLettersOut);
+        } else {
+          if (
+            solved.indexOf(letter) === -1 &&
+            lettersIn.indexOf(letter) === -1
+          ) {
+            setLettersIn((old) => `${old}${letter}`);
+          }
+          const temp = PLACE_HOLDER.split("");
+          temp[position - 1] = letter;
+          const newRegexNot = temp.join("");
+          if (!regexNot.includes(newRegexNot)) {
+            setRegexNot((old) => [...old, newRegexNot]);
+          }
+          removeFromState(letter, setLettersOut);
+        }
+        setLetter("");
+        setPosition("?");
+      }
+      ref.current.focus();
+    },
+    [position, letter, removeFromState, solved, lettersIn, regexNot]
+  );
 
   const hasnt = useCallback(() => {
     if (position && letter) {
-      if (position === "?") {
-        if (
-          solved.indexOf(letter) === -1 &&
-          lettersOut.indexOf(letter) === -1
-        ) {
-          setLettersOut((old) => `${old}${letter}`);
-        }
-      } else {
-        const temp = PLACE_HOLDER.split("");
-        temp[position - 1] = letter;
-        const newRegexNot = temp.join("");
-        setRegexNot((old) => [...old, newRegexNot]);
+      if (lettersOut.indexOf(letter) === -1) {
+        setLettersOut((old) => `${old}${letter}`);
       }
+      removeFromState(letter, setSolved, true);
+      removeFromState(letter, setLettersIn);
       setLetter("");
       setPosition("?");
     }
-  }, [letter, position, solved, lettersOut]);
+    ref.current.focus();
+  }, [position, letter, lettersOut, removeFromState]);
+
+  const removeFromRegexNot = useCallback((letterOrItem) => {
+    const letter = /[a-z]/.test(letterOrItem);
+    setRegexNot((old) => {
+      if (letter) {
+        return old.filter((item) => item !== letterOrItem);
+      } else {
+        return old.filter((item, i) => i !== letterOrItem);
+      }
+    });
+  }, []);
 
   return (
     <div className={styles.container}>
@@ -56,13 +118,14 @@ function Solver() {
       <h3>The word has those letters</h3>
       <span>Letter: </span>
       <input
+        ref={ref}
         type="text"
         maxLength={1}
-        style={{ width: "30px" }}
+        className={styles.letterInput}
         value={letter}
         onChange={(e) => {
-          if (/[a-z]{1}/.test(e.target.value)) {
-            return setLetter(e.target.value);
+          if (/[a-zA-Z]{1}/.test(e.target.value)) {
+            return setLetter(e.target.value.toLowerCase());
           }
         }}
       />
@@ -80,34 +143,62 @@ function Solver() {
           <span style={{ marginRight: "5px" }}>{pos}</span>
         </Fragment>
       ))}
-      <button type="button" onClick={has}>
-        {"has"}
-      </button>
-      <button type="button" onClick={hasnt}>
-        {"hasn't"}
-      </button>
-      <p>SOLVED: {solved}</p>
+      {position === "?" ? (
+        <>
+          <button type="button" onClick={hasnt}>
+            {"hasn't <Enter>"}
+          </button>
+        </>
+      ) : (
+        <>
+          <button type="button" onClick={() => has(true)}>
+            {"has right position <Insert>"}
+          </button>
+          <button type="button" onClick={() => has(false)}>
+            {"has wrong position <Delete>"}
+          </button>
+        </>
+      )}
+      <p>
+        SOLVED:{" "}
+        <LettersComponent
+          letters={solved}
+          onClick={handleRemove(setSolved, true)}
+          className={styles.correct}
+        />
+      </p>
       <p>
         Letters In:{" "}
-        {lettersIn.split("").map((l, i) => (
-          <span key={i} style={{ marginLeft: "5px" }}>
-            {l}
-          </span>
-        ))}
+        <LettersComponent
+          letters={lettersIn}
+          onClick={handleRemove(setLettersIn, false)}
+          className={styles.existent}
+        />
       </p>
       <p>
         Letters Out:{" "}
-        {lettersOut.split("").map((l, i) => (
-          <span key={i} style={{ marginLeft: "5px" }}>
-            {l}
-          </span>
-        ))}
+        <LettersComponent
+          letters={lettersOut}
+          onClick={handleRemove(setLettersOut, false)}
+          className={styles.letter}
+        />
       </p>
       <p>
         {"Patterns which doesn't match the word "}
         <ul>
           {regexNot.map((pattern, i) => (
-            <li key={i}>{pattern}</li>
+            <li
+              key={i}
+              onClick={() =>
+                setRegexNot((old) => old.filter((re) => re !== pattern))
+              }
+            >
+              <LettersComponent
+                letters={pattern}
+                onClick={() => null}
+                className={styles.notExistent}
+              />
+            </li>
           ))}
         </ul>
       </p>
