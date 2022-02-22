dab1975c47182ea6f0144d8afd0de3a1094bdfe2
Alex Gouvea Vasconcelos <alexgv@procempa.com.br>
2022-02-22 14:07:19 -0300

    Mostly, styles

--- pages/solver.js
+++ pages/solver.js
@@ -27,6 +27,11 @@ function Solver() {
   useKeypress("3", () => setPosition("3"));
   useKeypress("4", () => setPosition("4"));
   useKeypress("5", () => setPosition("5"));
+  useKeypress("6", () => setPosition("?"));
+  useKeypress("7", () => setPosition("?"));
+  useKeypress("8", () => setPosition("?"));
+  useKeypress("9", () => setPosition("?"));
+  useKeypress("0", () => setPosition("?"));
   useKeypress("?", () => setPosition("?"));
   useKeypress("Escape", () => {
     setPosition("?");
@@ -67,7 +72,7 @@ function Solver() {
         setInitial(false);
       }
     }
-  }, [initial, lettersIn.length, lettersOut.length, solved]);
+  }, [initial, lettersIn, lettersOut, solved]);
 
   useEffect(() => {
     const regex = new RegExp(solved, "i");
@@ -82,6 +87,14 @@ function Solver() {
       setWords(getWords(dic, regex, notRegex, inAux, outAux));
     }
   }, [dic, lettersIn, lettersOut, regexNot, solved, initial]);
+
+  useEffect(() => {
+    const newIn = regexNot
+      .map((p) => p.replace(/\./g, ""))
+      .reduce((acc, l) => (acc.indexOf(l) === -1 ? `${acc}${l}` : acc), "");
+    console.log("newIn: ", newIn);
+    setLettersIn(newIn);
+  }, [regexNot]);
 
   const removeFromState = useCallback((letter, setter, keepSlot) => {
     setter((old) =>
@@ -104,6 +117,8 @@ function Solver() {
     [removeFromState]
   );
 
+  useEffect(() => console.log("lettersIn: ", lettersIn), [lettersIn]);
+
   const has = useCallback(
     (rightPosition) => {
       if (position !== "?" && letter) {
@@ -116,12 +131,12 @@ function Solver() {
           removeFromState(letter, setLettersIn);
           removeFromState(letter, setLettersOut);
         } else {
-          if (
-            solved.indexOf(letter) === -1 &&
-            lettersIn.indexOf(letter) === -1
-          ) {
-            setLettersIn((old) => `${old}${letter}`);
-          }
+          // if (
+          //   solved.indexOf(letter) === -1 &&
+          //   lettersIn.indexOf(letter) === -1
+          // ) {
+          //   setLettersIn((old) => `${old}${letter}`);
+          // }
           const temp = PLACE_HOLDER.split("");
           temp[position - 1] = letter;
           const newRegexNot = temp.join("");
@@ -145,110 +160,140 @@ function Solver() {
       }
       removeFromState(letter, setSolved, true);
       removeFromState(letter, setLettersIn);
+      removeFromRegexNot(letter);
       setLetter("");
       setPosition("?");
     }
     ref.current.focus();
   }, [position, letter, lettersOut, removeFromState]);
 
+  const removeFromRegexNot = useCallback(
+    (letter) =>
+      setRegexNot((old) => old.filter((p) => p.indexOf(letter) === -1)),
+    [regexNot]
+  );
+
   return (
     <div className={styles.container}>
-      <div className={styles.rules}></div>
-      <h3>The word has those letters</h3>
-      <span>Letter: </span>
-      <input
-        ref={ref}
-        type="text"
-        maxLength={1}
-        className={styles.letterInput}
-        value={letter}
-        onChange={(e) => {
-          if (/[a-zA-Z]{1}/.test(e.target.value)) {
-            return setLetter(e.target.value.toLowerCase());
-          }
-        }}
-      />
-      <span style={{ marginLeft: "15px" }}>Position: </span>
-      {"12345?".split("").map((pos, i) => (
-        <Fragment key={i}>
+      <div className={styles.input}>
+        <h3 className={styles.innerTitle}>The word has those letters</h3>
+        <div className="letter">
+          <span>Letter: </span>
           <input
-            style={{ marginLeft: "15px" }}
-            type="radio"
-            name="pos"
-            value={pos}
-            checked={pos === position}
-            onChange={(e) => setPosition(e.target.value)}
+            ref={ref}
+            type="text"
+            maxLength={1}
+            className={styles.letterInput}
+            value={letter}
+            onChange={(e) => {
+              if (/[a-zA-Z]{1}/.test(e.target.value)) {
+                return setLetter(e.target.value.toLowerCase());
+              }
+            }}
           />
-          <span style={{ marginRight: "5px" }}>{pos}</span>
-        </Fragment>
-      ))}
-      {position === "?" ? (
-        <>
-          <button type="button" onClick={hasnt}>
-            {"hasn't <Enter>"}
-          </button>
-        </>
-      ) : (
-        <>
-          <button type="button" onClick={() => has(true)}>
-            {"has right position <Insert>"}
-          </button>
-          <button type="button" onClick={() => has(false)}>
-            {"has wrong position <Delete>"}
-          </button>
-        </>
-      )}
-      <div>
-        SOLVED:{" "}
-        <LettersComponent
-          letters={solved}
-          onClick={handleRemove(setSolved, true)}
-          className={styles.correct}
-        />
+        </div>
+        <div className="position">
+          <span style={{ marginLeft: "15px" }}>Position: </span>
+          {"12345?".split("").map((pos, i) => (
+            <Fragment key={i}>
+              <input
+                style={{ marginLeft: "15px" }}
+                id={`pos${i}`}
+                type="radio"
+                name="pos"
+                value={pos}
+                checked={pos === position}
+                onChange={(e) => setPosition(e.target.value)}
+              />
+              <label htmlFor={`pos${i}`} style={{ marginRight: "5px" }}>
+                {pos}
+              </label>
+            </Fragment>
+          ))}
+        </div>
+        <div className="filler"></div>
+        <div className={styles.toolbar}>
+          {position === "?" ? (
+            <>
+              <button type="button" className={styles.hasnt} onClick={hasnt}>
+                {"hasn't <Enter>"}
+              </button>
+            </>
+          ) : (
+            <>
+              <button
+                type="button"
+                className={styles.wrightSpot}
+                onClick={() => has(true)}
+              >
+                {"wright spot <Insert>"}
+              </button>
+              <button
+                type="button"
+                className={styles.wrongSpot}
+                onClick={() => has(false)}
+              >
+                {"wrong spot <Delete>"}
+              </button>
+            </>
+          )}
+        </div>
+        <div>SOLVED:</div>
+        <div>
+          <LettersComponent
+            letters={solved}
+            onClick={handleRemove(setSolved, true)}
+            className={styles.correct}
+          />
+        </div>
+        <div>Letters In:</div>
+        <div>
+          <LettersComponent
+            letters={lettersIn}
+            onClick={removeFromRegexNot}
+            className={styles.existent}
+          />
+        </div>
+        <div>Letters Out:</div>
+        <div>
+          <LettersComponent
+            letters={lettersOut}
+            onClick={handleRemove(setLettersOut, false)}
+            className={styles.notExistent}
+          />
+        </div>
       </div>
-      <div>
-        Letters In:{" "}
-        <LettersComponent
-          letters={lettersIn}
-          onClick={handleRemove(setLettersIn, false)}
-          className={styles.existent}
-        />
+      <div className={styles.patterns}>
+        <div>{"Patterns which doesn't match the word "}</div>
+        <ul>
+          {regexNot.map((pattern, i) => (
+            <li
+              key={i}
+              onClick={() =>
+                setRegexNot((old) => old.filter((re) => re !== pattern))
+              }
+            >
+              <LettersComponent
+                letters={pattern}
+                onClick={() => null}
+                className={styles.existent}
+              />
+            </li>
+          ))}
+        </ul>
       </div>
-      <div>
-        Letters Out:{" "}
-        <LettersComponent
-          letters={lettersOut}
-          onClick={handleRemove(setLettersOut, false)}
-          className={styles.letter}
-        />
+      <div className={styles.words}>
+        <div>
+          {initial
+            ? "Best words to start with:"
+            : `Possible words (${words.length})`}
+        </div>
+        <ul className={styles.wordList}>
+          {words.map((word) => (
+            <li key={word}>{word}</li>
+          ))}
+        </ul>
       </div>
-      <div>{"Patterns which doesn't match the word "}</div>
-      <ul>
-        {regexNot.map((pattern, i) => (
-          <li
-            key={i}
-            onClick={() =>
-              setRegexNot((old) => old.filter((re) => re !== pattern))
-            }
-          >
-            <LettersComponent
-              letters={pattern}
-              onClick={() => null}
-              className={styles.notExistent}
-            />
-          </li>
-        ))}
-      </ul>
-      <div>
-        {initial
-          ? "Best words to start with:"
-          : `Possible words (${words.length})`}
-      </div>
-      <ul className={styles.wordList}>
-        {words.map((word) => (
-          <li key={word}>{word}</li>
-        ))}
-      </ul>
     </div>
   );
 }
